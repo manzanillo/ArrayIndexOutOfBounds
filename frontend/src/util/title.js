abdf2c6803e8a45c374cc0f11a88f85ea68669e3
manzanillo <manza@outlook.de>
2019-11-12 12:45:41 +0100

    change title

--- frontend/src/util/title.js
+++ frontend/src/util/title.js
@@ -1,30 +1,26 @@
-function getTitle (vm) {
-  const { title } = vm.$options
-  if (title) {
-    return typeof title === 'function'
-      ? title.call(vm)
-      : title
-  }
+function getTitle(vm) {
+    const { title } = vm.$options;
+    if(title) {
+        return typeof title === 'function' ? title.call(vm) : title;
+    }
 }
 
 const serverTitleMixin = {
-  created () {
-    const title = getTitle(this)
-    if (title) {
-      this.$ssrContext.title = `Vue HN 2.0 | ${title}`
+    created() {
+        const title = getTitle(this);
+        if(title) {
+            this.$ssrContext.title = `QnA | ${title}`;
+        }
     }
-  }
-}
+};
 
 const clientTitleMixin = {
-  mounted () {
-    const title = getTitle(this)
-    if (title) {
-      document.title = `Vue HN 2.0 | ${title}`
+    mounted() {
+        const title = getTitle(this);
+        if(title) {
+            document.title = `QnA | ${title}`;
+        }
     }
-  }
-}
+};
 
-export default process.env.VUE_ENV === 'server'
-  ? serverTitleMixin
-  : clientTitleMixin
+export default process.env.VUE_ENV === 'server' ? serverTitleMixin : clientTitleMixin;
